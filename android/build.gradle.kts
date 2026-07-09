e43fbc3ac9a99b880dd81310262d4aa68261309d
talib344 <talibsindhi344@gmail.com>
2026-07-08 20:44:45 -0700

    Create build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.buildDir = file("../build")
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
