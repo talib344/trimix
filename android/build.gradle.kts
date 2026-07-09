f7e964bbd4a59d288990c23aae909c58b5aee2b6
talib344 <talibsindhi344@gmail.com>
2026-07-08 20:52:54 -0700

    Update Android Gradle and Kotlin plugin versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
 buildscript {
     repositories {
         google()
