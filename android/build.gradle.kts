270e919a0f6d00cc99d3b1c1687388843d437e7b
talib344 <talibsindhi344@gmail.com>
2026-07-08 20:58:44 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,29 +2,14 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
-rootProject.buildDir = file("../build")
-subprojects {
-    project.buildDir = file("${rootProject.buildDir}/${project.name}")
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
